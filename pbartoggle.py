cc2ccec7b290806ff182bd9261e7e9eb6628b42f
sawsuh <prao060@gmail.com>
2019-12-11 02:20:28 +1100

    updated script

--- pbartoggle.py
+++ pbartoggle.py
@@ -4,19 +4,18 @@ import subprocess as sp
 pidcmd = ['pgrep', '-a', 'polybar']
 pgrep = sp.run(pidcmd, stdout=sp.PIPE) #grep polybar pids
 processes = pgrep.stdout.decode('utf-8') # get output
-#barline = re.search("[0-9]+.+barski",processes).group(0) #find line corresponding to our bar
 barpid = processes.split(' ')[0] #bar pid 
 
 if barpid != "":
 	widcmd = ['xdotool', 'search', '--pid', str(barpid), '--onlyvisible']
 	xdowid = sp.run(widcmd, stdout=sp.PIPE)
-	wid = xdowid.stdout.decode('utf-8')
+	wid = xdowid.stdout.decode('utf-8') #get wid
 	if wid != "":
 		sp.call(['xdotool', 'windowunmap', str(wid)])
 		sp.call(['bspc', 'config', 'top_padding', '20'])
 	else:
 		sp.call(['bspc', 'config', 'top_padding', '70'])
-		xdowid = sp.run(widcmd[:4], stdout=sp.PIPE)
+		xdowid = sp.run(widcmd[:4], stdout=sp.PIPE) #get wid without visible flag
 		wid = xdowid.stdout.decode('utf-8')
 		sp.call(['xdotool', 'windowmap', str(wid)])
 else:
