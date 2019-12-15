28b326a56e82c475716ee2ae1406e718f50cc202
sawsuh <prao060@gmail.com>
2019-12-15 20:53:50 +1100

    made vim use ale fixers

--- scripts/python/pbartoggle.py
+++ scripts/python/pbartoggle.py
@@ -1,21 +1,24 @@
 import subprocess as sp
 
-pgrep = sp.run(['pgrep', '-a', 'polybar'], stdout=sp.PIPE) #grep polybar pids
-processes = pgrep.stdout.decode('utf-8') # get output
-barpid = processes.split(' ')[0] #bar pid
+pgrep = sp.run(["pgrep", "-a", "polybar"], stdout=sp.PIPE)  # grep polybar pids
+processes = pgrep.stdout.decode("utf-8")  # get output
+barpid = processes.split(" ")[0]  # bar pid
 
 if barpid:
-	widcmd = ['xdotool', 'search', '--pid', str(barpid), '--onlyvisible']
-	xdowid = sp.run(widcmd, stdout=sp.PIPE)
-	wid = xdowid.stdout.decode('utf-8') #get wid
-	if wid:
-		sp.run(['polybar-msg', 'cmd', 'hide'], stdout=sp.DEVNULL, stderr = sp.DEVNULL)
-		sp.run(['bspc', 'config', 'top_padding', '20'])
-	else:
-		sp.run(['bspc', 'config', 'top_padding', '70'])
-		xdowid = sp.run(widcmd[:4], stdout=sp.PIPE) #get wid without visible flag
-		wid = xdowid.stdout.decode('utf-8')
-		sp.run(['polybar-msg', 'cmd', 'show'], stdout=sp.DEVNULL, stderr = sp.DEVNULL)
+    widcmd = ["xdotool", "search", "--pid", str(barpid), "--onlyvisible"]
+    xdowid = sp.run(widcmd, stdout=sp.PIPE)
+    wid = xdowid.stdout.decode("utf-8")  # get wid
+    if wid:
+        sp.run(["polybar-msg", "cmd", "hide"],
+               stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+        sp.run(["bspc", "config", "top_padding", "20"])
+    else:
+        sp.run(["bspc", "config", "top_padding", "70"])
+        # get wid without visible flag
+        xdowid = sp.run(widcmd[:4], stdout=sp.PIPE)
+        wid = xdowid.stdout.decode("utf-8")
+        sp.run(["polybar-msg", "cmd", "show"],
+               stdout=sp.DEVNULL, stderr=sp.DEVNULL)
 else:
-	sp.run(['bspc', 'config', 'top_padding', '70'])
-	sp.run(['polybar', 'barski'], stdout=sp.DEVNULL, stderr = sp.DEVNULL)
+    sp.run(["bspc", "config", "top_padding", "70"])
+    sp.run(["polybar", "barski"], stdout=sp.DEVNULL, stderr=sp.DEVNULL)
