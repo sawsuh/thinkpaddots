5af10153764794bf3fcdd66c75a107f26bd9dfcf
sawsuh <prao060@gmail.com>
2019-12-23 14:57:30 +1100

    'made files job'

---
+++ scripts/python/files.py
@@ -0,0 +1,18 @@
+import datetime
+import os
+import shutil
+
+os.chdir("/home/prash/Downloads")
+for file in os.listdir():
+    modtime = datetime.datetime.fromtimestamp((os.path.getmtime(file)))
+    timeago = datetime.datetime.now() - modtime
+    daysago = int(timeago.days)
+    if daysago > 7:
+        if os.path.isfile(file):
+            os.remove(file)
+        else:
+            shutil.rmtree(file)
+        print(
+            f'{file}, removed at {datetime.datetime.now().strftime("%d/%m/%Y")} last modified {modtime.strftime("%d/%m/%Y")}'
+        )
+        continue
